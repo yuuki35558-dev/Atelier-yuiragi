ddc1952d6be649b401f83829c3bfb2540818e1f8
yuuta <yuuta@example.com>
2026-02-04 02:10:41 +0900

    Fix base with trailing slash

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://yuuki35558-dev.github.io',
-  base: '/Atelier-yuiragi',   // ← これ！！！
+  base: '/Atelier-yuiragi/',   // ← ★末尾スラッシュ必須
   output: 'static',
   build: {
     outDir: 'docs',
